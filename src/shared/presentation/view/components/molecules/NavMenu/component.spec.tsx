1d7da23e3bd83c143dd1f9a7d10dc9fad4c64fb2
Raul Mendes Rosá <raul.hmetal@gmail.com>
2022-07-17 15:55:54 -0300

    test: navmenu

---
+++ src/shared/presentation/view/components/molecules/NavMenu/component.spec.tsx
@@ -0,0 +1,39 @@
+import { render, screen } from '@testing-library/react';
+
+import { ThemeProvider } from '@/shared/presentation/contexts';
+
+import NavMenu from './index';
+
+describe('NavMenu', () => {
+    it('should render the component correctly, displaying all the options', () => {
+        render(
+            <NavMenu
+                items={[
+                    {
+                        href: '/',
+                        icon: 'home',
+                        text: 'Home',
+                    },
+                    {
+                        href: '/products',
+                        icon: 'list',
+                        text: 'Products',
+                    },
+                    {
+                        href: '/customers',
+                        icon: 'people',
+                        text: 'Customers',
+                    },
+                ]}
+            />,
+            {
+                wrapper: ThemeProvider,
+            },
+        );
+
+        expect(screen.getByText('Home')).toBeInTheDocument();
+        expect(screen.getByText('Products')).toBeInTheDocument();
+        expect(screen.getByText('Customers')).toBeInTheDocument();
+        expect(screen).toMatchSnapshot();
+    });
+});
