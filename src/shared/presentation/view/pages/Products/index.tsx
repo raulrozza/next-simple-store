ef43cfa67d0aae164d1182ec852eab771634b9d7
Raul Mendes Rosá <raul.hmetal@gmail.com>
2022-07-18 21:25:45 -0300

    feat: fetching products

--- src/shared/presentation/view/pages/Products/index.tsx
+++ src/shared/presentation/view/pages/Products/index.tsx
@@ -1,27 +1,13 @@
 import { FC } from 'react';
 
-import { IProduct } from '@/shared/domain/entities/Product';
 import { ProductInfoItem } from '@/shared/presentation/view/components/molecules';
 import { Menu } from '@/shared/presentation/view/components/organisms';
 import { EntityManagerList } from '@/shared/presentation/view/components/templates';
 
+import { useProductsController } from './hooks';
+
 const Products: FC = () => {
-    const products: IProduct[] = [
-        {
-            id: 'dshf9sdghsd',
-            name: 'Product 1',
-            description: 'Description 1 of the initial product named 1',
-            price: 100.99,
-            slug: 'product-1',
-        },
-        {
-            id: 'dshf9gfgfdgdhsd',
-            name: 'Product 2',
-            description: 'Description 2 of the initial product named 2',
-            price: 200.99,
-            slug: 'product-2',
-        },
-    ];
+    const { products } = useProductsController();
 
     return (
         <section>
